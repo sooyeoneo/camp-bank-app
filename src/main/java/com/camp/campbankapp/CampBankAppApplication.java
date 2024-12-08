84e19822f011016a8711b8480a438cf615a8ad1d
Robbie <robbie@>
2024-12-08 19:43:37 +0900

    init

---
+++ src/main/java/com/camp/campbankapp/CampBankAppApplication.java
@@ -0,0 +1,13 @@
+package com.camp.campbankapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CampBankAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CampBankAppApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/camp/campbankapp/CampBankAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.camp.campbankapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CampBankAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
